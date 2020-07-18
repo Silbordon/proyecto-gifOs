9589bcf7524349802d74fe0a42a92c5f12ccf312
Silvina Bordon <silbordon.89@gmail.com>
2020-07-18 11:47:33 -0300

    Se agregan estilos al cronometro, sigue sin funcionar

--- src/video.js
+++ src/video.js
@@ -5,8 +5,8 @@ let captura = document.getElementById('contenedorcaptura');
 let seccionInstrucciones = document.getElementById('contenedor-instrucciones');
 let seccionVideo = document.getElementById('contenedor-video');
 let grabando = document.getElementById('contenedorlisto');
-let tituloantes =document.getElementById('tituloAntes');
-let titulocaptura =document.getElementById('tituloCaptura');
+let tituloantes = document.getElementById('tituloAntes');
+let titulocaptura = document.getElementById('tituloCaptura');
 
 comenzar.addEventListener('click', () => {
     seccionInstrucciones.classList.add('display-none');
@@ -24,49 +24,74 @@ function getStreamAndRecord() {
             height: 510,
         }
     })
-    .then(function (stream) {
-        recorder = RecordRTC(stream, {
-            type: 'gif',
-            frameRate: 1,
-            quality: 10,
-            width: 360,
-            hidden: 240,
+        .then(function (stream) {
+            recorder = RecordRTC(stream, {
+                type: 'gif',
+                frameRate: 1,
+                quality: 10,
+                width: 360,
+                hidden: 240,
 
-            onGifRecordingStarted: function () {
-                console.log('started')
-            },
-        });
-        video.srcObject = stream;
-        video.play();
-    }).catch((e) => {
+                onGifRecordingStarted: function () {
+                    console.log('started')
+                },
+            });
+            video.srcObject = stream;
+            video.play();
+        }).catch((e) => {
         console.log(e);
     })
 }
 
-captura.addEventListener('click',() => {
+
+captura.addEventListener('click', () => {
     captura.classList.add('display-none');
     grabando.classList.remove('display-none');
     tituloantes.classList.add('display-none');
     titulocaptura.classList.remove('display-none');
-    
+
 })
+
 
 captura.addEventListener('click', async function f() {
 
     recorder.startRecording();
+    const sleep = m => new Promise(r => setTimeout(r, m));
+    await sleep(3000);
     console.log("Start recording");
 })
-   
-   /*const sleep = m => new Promise(r => setTimeout(r, m));
-    await sleep(3000);
-    
-    recorder.stopRecording(function() {
-        let blob = recorder.getBlob();
-        invokeSaveAsDialog(blob);
-    });
 
+recorder.stopRecording(function () {
+    let blob = recorder.getBlob();
+    invokeSaveAsDialog(blob);
 });
+
+
+/*Funcion cronometro
+
+let segundos = 0;
+let minutos = 0;
+let horas = 0;
+
+
+function f() {
+    setInterval(function () {
+        segundos++;
+        mostrar(segundos)
+    }, 1000)
+}
+
+let seg = document.getElementById('screen')
+function mostrar(segundos) {
+    seg.innerText = segundos
+}
+
+ 
 */
+ 
+
+ 
+ 
 
 
 
